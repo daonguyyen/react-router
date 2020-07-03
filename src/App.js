d45f3df5c4f626a3ecf845b082cd8ad59331855d
daonguyyen <mdnguyen928@gmail.com>
2020-07-03 14:54:22 +0700

    1-install React-router-dom,import and using router, route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <div className="App">
+
+        {/* Menu */}
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+        </ul>
+        {/* Content */}
+        <Route exact path="/"><Home /></Route>
+        <Route path="/about"> <About /> </Route>
+      </div>
+    </Router>
   );
 }
 
