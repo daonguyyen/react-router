2387dc6ce61450cd9c67e9f75c1f0863701abe27
daonguyyen <mdnguyen928@gmail.com>
2020-07-03 16:49:21 +0700

    5-Custom Link

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom'
-import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Home from './components/Home';
+
+const MenuLink = ({label, to, activeOnlyWhenExact}) => {
+  return (
+    <Route path={to} exact={activeOnlyWhenExact} children={ ({ match }) => {
+        var active = match ? 'active testClass' : '';
+        return (
+          <li className={active}>
+            <Link to={to}>{label}</Link>
+          </li>
+        ) 
+    }}/>
+  )
+}
+
+
 function App() {
   return (
     <Router>
 
       <div className="App">
-
+          
         {/* Menu */}
-        <ul class="nav justify-content-center">
-          <li class="nav-item">
-            <NavLink exact to="/" activeClassName="active">Home</NavLink>
-          </li>
-          <li class="nav-item">
-            <NavLink to="/about" activeClassName="active">About</NavLink>
-          </li>
-          <li class="nav-item">
-            <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-          </li>
+        <ul className="nav justify-content-center">
+          <MenuLink label="Home" to="/" activeOnlyWhenExact={true}/>
+          <MenuLink label="About" to="/about"/>
+          <MenuLink label="Contact" to="/contact"/>
         </ul>
         {/* Content */}
         <Route exact path="/"><Home /></Route>
@@ -32,3 +41,5 @@ function App() {
 }
 
 export default App;
+
+//Custom thi dung Link, su dung co san thi dung NavLink
